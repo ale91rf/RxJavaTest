44f50fbdc4cca7fab3e361b9b3bf464659c9e0e9
Ale Ramírez Fernández <alejandro@MacBook-Pro-de-Ale.local>
2016-04-30 19:50:06 +0200

    mainActivity implemented

--- app/src/main/java/com/alefernandez/rxjavatest/ui/MainActivity.java
+++ app/src/main/java/com/alefernandez/rxjavatest/ui/MainActivity.java
@@ -2,11 +2,30 @@ package com.alefernandez.rxjavatest.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alefernandez.rxjavatest.R;
 import com.alefernandez.rxjavatest.model.Post;
 
+import butterknife.Bind;
+
 public class MainActivity extends AppCompatActivity implements MainView{
+
+    @Bind(R.id.btnStart)
+    Button mBtnStart;
+
+    @Bind(R.id.pBarDownloading)
+    ProgressBar mProgress;
+
+    @Bind(R.id.lblTitle)
+    TextView mLblTitle;
+
+    @Bind(R.id.lblBody)
+    TextView mLblBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,26 +35,27 @@ public class MainActivity extends AppCompatActivity implements MainView{
 
     @Override
     public void showProgressBar() {
-
+        mProgress.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hideProgressBar() {
-
+        mProgress.setVisibility(View.INVISIBLE);
     }
 
     @Override
     public void setEnabledButton(boolean aEnabled) {
-
+        mBtnStart.setEnabled(aEnabled);
     }
 
     @Override
     public void displayPost(Post aPost) {
-
+        mLblTitle.setText(aPost.getTitle());
+        mLblBody.setText(aPost.getBody());
     }
 
     @Override
     public void showMessage(String aMessage) {
-
+        Toast.makeText(getApplicationContext(), aMessage, Toast.LENGTH_LONG).show();
     }
 }
